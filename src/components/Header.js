d4ec866ff2a4d14cf325bfc2ca6a1869d0236862
A1exsandro <alexsandro.acp@gmail.com>
2022-06-28 22:36:30 -0400

    feat: component Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Header() {
+
+  return (
+    <>
+      <div className='header'>
+        <h1>Todos List</h1>
+      </div>
+    </>
+  );
+ 
+}
+
+export default Header;
