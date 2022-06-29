84e1e35e774b01df829191dd805d4ba3d8627ab0
A1exsandro <alexsandro.acp@gmail.com>
2022-06-28 22:35:09 -0400

    feat: change App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import Home from './components/Home';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import './App.css';
+import TodosList from './components/TodosList';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
-   <Home />
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div>
+          <Header />
+        </div>      
+        <div>
+          <Form 
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        </div>
+        <div>
+          <TodosList 
+            todos={todos}
+            setTodos={setTodos}
+          />
+        </div>
+      </div>
+    </div>
+   
   );
 }
 
